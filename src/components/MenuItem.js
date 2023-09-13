7978ef74d5646ce20821874f30e4e57aec0fb00b
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-14 00:26:28 +0300

    sidebar

---
+++ src/components/MenuItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MenuItem = ( {icon, value} ) => {
+    return (
+        <>
+            <FontAwesomeIcon icon={faHome} />
+            <p>Home</p>
+        </>
+    )
+}
+
+export default MenuItem;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Logo from "./Logo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays, faHome, faRightFromBracket, faTelevision, faVideoCamera } from "@fortawesome/free-solid-svg-icons";
+import MenuItem from "./MenuItem";
 
 const SideBar = () => {
     return (
-        <>
+        <div className="sidebar">
             <Logo />
-        </>
+            <MenuItem icon={faHome} value={"Home"} />
+            <MenuItem icon={faVideoCamera} value={"Movies"} />
+            <MenuItem icon={faTelevision} value={"TV Series"} />
+            <MenuItem icon={faCalendarDays} value={"Upcoming"} />
+
+            <div className="alert">
+                <h4>Play movie quizes and earn free tickets</h4>
+                <p className="gray">50k people are playing now</p>
+                <button>Start Playing</button>
+            </div>
+            
+            <MenuItem icon={faRightFromBracket} value={"Log Out"} />
+        </div>
     )
 }
