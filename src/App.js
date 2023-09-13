977b4015e51d02d6392a073b1720741c3349c2b0
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-13 03:42:54 +0300

    getting data and styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import './components/MovieGrid.js';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import MovieGrid from './components/MovieGrid';
+import React , { useState, useEffect } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState([])
+
+  const getMovieRequest = async () => {
+    const url = "https://api.themoviedb.org/3/movie/top_rated?api_key=fd14b8822352d61899d7c4f5f76fe82a"
+  
+    const response = await fetch(url)
+    const responseJSON = await response.json()
+    setMovies(responseJSON.results)
+  }
+  
+  useEffect(() => {
+    getMovieRequest()
+  }, [])
   return (
     <div className="App">
-      <div className='"'>
+      <div className='d-flex m-3 title'>
         <h2>Featured Movies</h2>
-        <a>See More</a>
+        <a href='#' >See More</a>
       </div>
-    <MovieGrid />
+    <MovieGrid  movies={movies}/>
     </div>
   );
 }
--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -1,17 +1,17 @@
 import React  from "react";
 
 const MovieGrid = ( {movies} ) => {
-    return <>
+    return <section className="d-flex m-3 movies" >
         {movies.map((movie, index) => {
             if(index < 10){
-                return <div className="card" data-testid="movie-card" id={movie.id}>
-                    <img src={movie.poster_path}  data-testid="movie-poster"/>
-                    <p data-testid="movie-release-date">{movie.release_date}</p>
-                    <h3 data-testid="movie-title">{movie.title}</h3>
+                return <div className="card d-flex justify-content-start flex-direction-column" data-testid="movie-card" id={movie.id}>
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}  data-testid="movie-poster"/>
+                    <p className="gray" data-testid="movie-release-date">{movie.release_date}</p>
+                    <h4 data-testid="movie-title">{movie.title}</h4>
                 </div>
             }
         })}
-    </>
+    </section>
 }
 
 export default MovieGrid
