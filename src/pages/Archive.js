0dfada3b766c20fdf2cfadf966fe9db238f7e9d0
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-06 16:49:59 +0300

    archive pages

--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -4,11 +4,11 @@ import MovieGrid from "../components/MovieGrid";
 import AddFavorite from "../components/AddFavorite";
 import Footer from "../components/Footer";
 
-const Home = () => {
+const Archive = ({ query, title, type }) => {
     const [movies, setMovies] = useState([])
 
     const getMovieRequest = async () => {
-        const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}`
+        const url = `https://api.themoviedb.org/3/${query}?api_key=${process.env.REACT_APP_API_KEY}`
 
         const response = await fetch(url)
         const responseJSON = await response.json()
@@ -22,10 +22,10 @@ const Home = () => {
     return (
         <div className="App">
             <HeroSection />
-            <MovieGrid movies={movies} favoriteComponent={AddFavorite} title={"Featured Movies"} type={"movie"} />
+            <MovieGrid movies={movies} favoriteComponent={AddFavorite} title={title} type={type} />
             <Footer />
         </div>
     );
 }
 
-export default Home;
+export default Archive;
